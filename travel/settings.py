7b8c2b8c16ebda3086859aa243358676083723df
Simon Chanzu <chanzusimon6@gmail.com>
2025-06-15 20:26:59 +0300

    commits

--- travel/settings.py
+++ travel/settings.py
@@ -37,9 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'listings',
-    'rest_framework',
-    'rest_framework.authtoken',
+
 ]
 
 MIDDLEWARE = [
